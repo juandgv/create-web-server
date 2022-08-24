799cc5513ed4accb7a2ca0744c2e313d9f9082d7
Juan David Gil Valencia <juandagilva85@gmail.com>
2022-08-24 18:48:48 -0500

    Empezando a crear un servidor con Node.js

---
+++ index.js
@@ -0,0 +1,12 @@
+console.clear();
+import {createServer} from 'http'; // clases y metodos para manejar HTTP
+
+const httpServer = createServer(
+    (req, res) => {
+        console.log('peticion recibida!')
+        res.end("Ya te recibi PESAO")
+    }
+); //creamos el servidor
+
+httpServer.listen(3520);
+
