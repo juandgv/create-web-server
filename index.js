5588a12a6293e586031c3f37e4dda35e628e6093
Juan David Gil Valencia <juandagilva85@gmail.com>
2022-08-24 23:42:18 -0500

    Entendiendo una request http en nodejs

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ import {createServer} from 'http'; // clases y metodos para manejar HTTP
 
 const httpServer = createServer(
     (req, res) => {
-        console.log('peticion recibida!')
-        res.end("Ya te recibi PESAO")
-        
+        //console.log(req.method);  // Nos falta el metodo para indicar que quiere el cliente
+        //console.log(req.url);  // Nos falta el path/ruta para identificar el recurso
+        //console.log(req.headers); // Nos faltan las cabeceras
+        //nos falta el body/payload
+        let data = '';
+        let chunkIndex = 0;
+        req.on("data", (chunk) => {
+            data += chunk;
+            chunkIndex++;
+            console.log(chunkIndex)
+        });
+         
+
+        req.on('end', () =>{
+            console.log(data); 
+            res.end("Ya te recibi pesao")
+
+        });
     }
 ); //creamos el servidor
 
